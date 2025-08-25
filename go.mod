9d79c1ca6a85270ac95ba7e9982ae26324a9c800
nonatee <inctut7@gmail.com>
2025-08-25 22:20:09 +0300

    ebiten implemented

--- go.mod
+++ go.mod
@@ -3,6 +3,26 @@ module packetgraph
 go 1.25.0
 
 require (
+	codeberg.org/go-fonts/liberation v0.5.0 // indirect
+	codeberg.org/go-latex/latex v0.1.0 // indirect
+	codeberg.org/go-pdf/fpdf v0.10.0 // indirect
+	git.sr.ht/~sbinet/gg v0.6.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/goccmack/gocc v0.0.0-20230228185258-2292f9e40198 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.8.8 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/image v0.25.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/tools v0.26.0 // indirect
+	gonum.org/v1/plot v0.15.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,25 +2,64 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"log"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+type Game struct {
+	packetChan chan gopacket.Packet
+	ipMap      map[string]string
+}
+
+func (g *Game) Update() error {
+	select {
+	case packet := <-g.packetChan:
+		if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
+			dns, _ := dnsLayer.(*layers.DNS)
+			if !dns.QR { // query
+				for _, q := range dns.Questions {
+					fmt.Println("DNS Query:", string(q.Name))
+				}
+			} else { // response
+				for _, ans := range dns.Answers {
+					fmt.Println("DNS Response:", string(ans.Name), ans.IP)
+					g.ipMap[ans.IP.String()] = string(ans.Name)
+				}
+			}
+		}
+	default:
+		// no packet available right now → just continue
+	}
+	return nil
+}
+func (g *Game) Draw(screen *ebiten.Image) {
+	vector.DrawFilledCircle(screen, 100, 100, 50, color.RGBA{255, 0, 0, 255}, true)
+	vector.StrokeLine(screen, 50, 50, 200, 200, 2, color.RGBA{0, 0, 255, 255}, true)
+}
+func (g *Game) Layout(w, h int) (int, int) { return 400, 400 }
 func main() {
-	handle, err := pcap.OpenLive(`\Device\NPF_{CC3C6DC2-EA3C-4A08-AFBA-D3EB3E811FD5}`, 1600, true, pcap.BlockForever) // replace "en0" with your interface
+	ebiten.SetWindowSize(400, 400)
+	ebiten.SetWindowTitle("Circles & Lines")
+	handle, err := pcap.OpenLive(`\Device\NPF_{CC3C6DC2-EA3C-4A08-AFBA-D3EB3E811FD5}`, 1600, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Close()
-	var counter = 0
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		fmt.Println(packet) // prints raw packet info
-		counter++
-		if counter > 5 {
-			close(packetSource.Packets())
-		}
+	game := &Game{
+		ipMap:      make(map[string]string),
+		packetChan: make(chan gopacket.Packet, 100), // buffer
 	}
+	go func() {
+		for packet := range packetSource.Packets() {
+			game.packetChan <- packet
+		}
+	}()
+	ebiten.RunGame(game)
 }
